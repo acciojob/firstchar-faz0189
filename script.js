279b3e04ec86349085583f45e04831c371490d05
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-11 02:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  for (let char of str){
+	  if(char! == " "){
+		  return " ";
+	  }
+  }
+	return "";
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
