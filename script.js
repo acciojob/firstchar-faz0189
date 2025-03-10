f2631546b302514135774767343199f000e080b6
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-11 02:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function firstChar(str) {
-  for (let char of str){
-	  if(char! == " "){
-		  return char;
-	  }
+  for (let char of str) {
+    if (char !== ' ') {
+      return char; // Return the first non-space character
+    }
   }
-	return "";
+  return ''; // Return an empty string if no non-space character is found
 }
+
 
